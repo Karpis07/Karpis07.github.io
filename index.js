7f45031973be2d369c1a9cc58da8d52f76063283
Karpis <akarpis@bk.ru>
2023-02-11 14:49:04 +0400

    sa

--- index.js
+++ index.js
@@ -1,59 +1,6 @@
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log('sad')
 
-const Quest = [
-    {
-    title:'5+5',
-    variant:['0','10','5'],
-    answer: 1,
-  },
-    {
-      title:'10*10',
-      variant:['100','10','1000'],
-      answer: 0,
-    },
-    {
-      title:'200 * 0.5',
-      variant:['100','10','50'],
-      answer: 0,
-    },
-  ]
-  
-  
-  const Result = ({corrent}) => {
-  
-    return (
-      <div>
-        <p>Chisht patasxanner{corrent}/3</p>
-      </div>
-    );
-  };
-  const Game = ({question , clickk}) => {
-  
-    return (
-      <div>
-    
-          <h1>{question.title}</h1>
-           {
-             question.variant.map((text,index) =>(
-              
-              <div style={{
-                display:'flex'
-              }}>
-                
-                <button onClick={()=> clickk(index)}>{text}</button>
-              </div>
-             ))
-  
-             
-           }
-      </div>
-    );
-  };
-  
-  
-  
   
   function App() {
     
@@ -76,13 +23,7 @@ const Quest = [
   
     return (
       <div>
-     {
-      step != Quest.length ? (
-          <Game question={question} clickk={clickk} />
-      ) :<Result corrent={corrent} />
-     }
-            
-            
+        
       </div>
     );
   }
